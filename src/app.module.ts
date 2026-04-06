033a468934bf2c60f9205a0c39d3fc69f727e80b
𝓑𝓛𝓐𝓧𝓚  𝓜𝓘𝓡𝓐 <blaxkmira128@gmail.com>
2026-04-07 00:48:01 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CalculatorController } from './calculator/calculator.controller';
+import { CalculatorService } from './calculator/calculator.service';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+@Module({
+  imports: [],
+  controllers: [AppController, CalculatorController],
+  providers: [AppService, CalculatorService],
+})
+export class AppModule {}
